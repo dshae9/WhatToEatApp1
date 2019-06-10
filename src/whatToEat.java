7f10865ee14d9346bd741a03f320991676f88b7e
Demonshae Glover <dshaeglover@gmail.com>
2019-06-10 10:44:03 -0400

    uploading modified code so that program runs

--- src/whatToEat.java
+++ src/whatToEat.java
@@ -1,35 +1,23 @@
 import java.util.Scanner;
+
 public class whatToEat {
- public static void main(String[] args) {
+public static void main(String[] args) {
 	 
-	 Scanner scnr = new Scanner(System.in);
-	var eventType = scnr.nextLine();
-	var partySize = scnr.nextLine();
+	Scanner scnr = new Scanner(System.in);
+    System.out.println("Enter event type: ");
+	String eventType = scnr.nextLine();
+    System.out.println("Enter party size: ");
+    int partySize;
+    partySize = scnr.nextInt();
 	
+    System.out.println("If your " + eventType + " event is for " + partySize );
 	
-	if(eventType = "casual") {
-		var foodType = "sandwiches made in the microwave.";
-	}if(eventType = "semi-formal") {
-		var foodType = "fried chicken in your kitchen.";
-	}if(eventType = "formal") {
-		var foodType = "chicken parmasean by a caterer.";
+	if(partySize <= 4) {
+		System.out.println(" , a small group will eat sandwiches from Jimmy Johns.");
+	}else if(partySize <= 10) {
+		System.out.println(" , a medium sized group will eat fried chicken from your kitchen.");
+	}else if(partySize >= 13) {
+		System.out.println(" , a large group will eat chicken parmasean from a caterer.");
 	}
-	if(partySize <= 1) {
-		System.out.println("a small group");
-	}if(partySize <= 12) {
-		System.out.println("a medium sized group");
-	}if(partySize <= 13) {
-		System.out.println("a large group");
-	}
-	
-	
-	
-	
-	
-	
-	System.out.println("Since you're hosting a " + eventType +
-			" for " + partySize + "you should serve" + foodType);
-			
-			
-			
-}}
+
+ }}
